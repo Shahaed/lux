48efc3890cc91cc77a78bd6e2eef68c167663ed1
Shahaed Hasan <shahaed77@gmail.com>
2019-01-05 16:37:10 -0600

    Testing CSS structure. New login component

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React Bad Formating
+            Learn React
           </a>
         </header>
       </div>
--- src/Login.tsx
+++ src/Login.tsx
@@ -1,9 +1,22 @@
-import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import React, { Component, Props } from "react";
+import "./Login.css";
 
 export default class Login extends Component {
+  constructor(props: Props<any>) {
+    super(props);
+  }
+
+  handleClick() {
+    alert("yo");
+  }
+
   render() {
     return (
       <div className="Login">
+        <Button variant="contained" color="primary" onClick={this.handleClick}>
+          Hello World
+        </Button>
         <form>
           <input type="text" name="firstname" value="Mickey" />
           <input type="text" name="lastname" value="Mouse" />
